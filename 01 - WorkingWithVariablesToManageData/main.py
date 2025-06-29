d0495922515ace2ff469284ba5da4ead33005346
WeBstwr <ifedhawebster@gmail.com>
2025-06-29 15:20:27 +0300

    Create Band Name Generator
    The program generates a band name based on the queries give like your
    city name and pet.
    It utilizes things like strings, input function, variables and
    concatenation.

--- 01 - WorkingWithVariablesToManageData/main.py
+++ 01 - WorkingWithVariablesToManageData/main.py
@@ -1,3 +1,14 @@
 # input() will get user input in console
-# Then print() will print the word "Hello" and the user input,=.
-print("Hello " + input("What is your name?"))
+# Then print() will print the word "Hello" and the user input.
+
+# print("Hello " + input("What is your name?"))
+
+# print(len(input("What is your name? ")))
+
+#VARIABLES
+# name = input("What is your name? ")
+# print(name)
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
